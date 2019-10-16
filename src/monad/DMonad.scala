c415998de63fa6a71769800ad4ed609fff0d2221
Takahashi Kazuya <kaz.t2497.bz@gmail.com>
2019-10-16 16:14:10 +0900

    modify dual tree

---
+++ src/monad/DMonad.scala
@@ -0,0 +1,31 @@
+package matching.monad
+
+trait DMonad[M[_,_]] {
+  def unit[A,B](b: B): M[A,B]
+  def bindl[A,B,C](m: M[A,B], f: A => M[C,C]): M[C,B]
+  def bindr[A,B,C](m: M[A,B], f: B => M[A,C]): M[A,C]
+  def success[A,B]: M[A,B]
+  def fail[A,B]: M[A,B]
+  def plus[A,B](m1: M[A,B], m2: M[A,B]): M[A,B]
+  def assert[A,B](m1: M[A,A], m2: M[A,B]): M[A,B]
+  def assertNot[A,B](m1: M[A,A], m2: M[A,B]): M[A,B]
+
+  def eval[A](m: M[A,A])(assignment: A => Boolean): Boolean
+  def evalr[A](m: M[Nothing,A])(v: A => Boolean): Boolean
+
+  def leaves[A](m: M[A,A]): Set[A]
+
+  final def apply[B](b: B) = unit(b)
+}
+
+object DMonad {
+  implicit class DMonadOp[M[_,_],A,B](self: M[A,B])(implicit m: DMonad[M]) {
+    def `>>=l`[C](f: A => M[C,C]) = m.bindl(self, f)
+    def `>>=r`[C](f: B => M[A,C]) = m.bindr(self, f)
+    def ++(m2: M[A,B]) = m.plus(self,m2)
+  }
+
+  implicit class DMonadAbbrev[M[_,_],A](self: M[A,A])(implicit m: DMonad[M]) {
+    def >>=[B](f: A => M[B,B]) = self `>>=l` f `>>=r` f
+  }
+}
--- src/monad/DTree.scala
+++ src/monad/DTree.scala
@@ -1,58 +1,81 @@
 package matching.monad
 
-import EPAMonad._
+import DMonad._
 
-sealed trait DTree[+A,+B]
+sealed trait DTree[A,B]
 case class DLeaf[A,B](b: B) extends DTree[A,B]
-case object DSuccess extends DTree[Nothing,Nothing]
-case object DFail extends DTree[Nothing,Nothing]
+case class DSuccess[A,B]() extends DTree[A,B]
+case class DFail[A,B]() extends DTree[A,B]
 case class DOr[A,B](l: DTree[A,B], r: DTree[A,B]) extends DTree[A,B]
 case class DAssert[A,B](l: DTree[A,A], r: DTree[A,B]) extends DTree[A,B]
 case class DAssertNot[A,B](l: DTree[A,A], r: DTree[A,B]) extends DTree[A,B]
 case class DLft[A,B](l: DTree[A,B]) extends DTree[A,B]
 
 object DTree {
-  implicit object DTreeMonad extends EPAMonad[DTree] {
+  implicit object DTreeMonad extends DMonad[DTree] {
     def unit[A,B](b: B) = DLeaf(b)
-    def bind[A,B,C,D](m: DTree[A,B], f: A => DTree[C,C], g: B => DTree[C,D]) = {
+    def bindl[A,B,C](m: DTree[A,B], f: A => DTree[C,C]) = {
       m match {
-        case DLeaf(b) => g(b)
-        case DSuccess => DSuccess
-        case DFail => DFail
-        case DOr(l,r) => DOr(l >>= (f,g), r >>= (f,g))
-        case DAssert(l,r) => DAssert(l >>= (f,f), r >>= (f,g))
-        case DAssertNot(l,r) => DAssertNot(l >>= (f,f), r >>= (f,g))
+        case DLeaf(b) => DLeaf(b)
+        case DSuccess() => DSuccess()
+        case DFail() => DFail()
+        case DOr(l,r) => DOr(l `>>=l` f, r `>>=l` f)
+        case DAssert(l,r) => DAssert(l >>= f, r `>>=l` f)
+        case DAssertNot(l,r) => DAssertNot(l >>= f, r `>>=l` f)
         case DLft(_) => throw new Exception("bind operetor is undefined for DLft.")
       }
     }
 
-    def success[A,B] = DSuccess
-    def fail[A,B] = DFail
+    def bindr[A,B,C](m: DTree[A,B], f: B => DTree[A,C]) = {
+      m match {
+        case DLeaf(b) => f(b)
+        case DSuccess() => DSuccess()
+        case DFail() => DFail()
+        case DOr(l,r) => DOr(l `>>=r` f, r `>>=r` f)
+        case DAssert(l,r) => DAssert(l, r `>>=r` f)
+        case DAssertNot(l,r) => DAssertNot(l, r `>>=r` f)
+        case DLft(_) => throw new Exception("bind operetor is undefined for DLft.")
+      }
+    }
+
+    def success[A,B] = DSuccess()
+    def fail[A,B] = DFail()
     def plus[A,B](m1: DTree[A,B], m2: DTree[A,B]) = DOr(m1,m2)
     def assert[A, B](m1: DTree[A,A],m2: DTree[A,B]) = DAssert(m1,m2)
     def assertNot[A, B](m1: DTree[A,A],m2: DTree[A,B]) = DAssertNot(m1,m2)
 
-    def toSet[A](m: DTree[Nothing,A]) = {
+    def eval[A](m: DTree[A,A])(v: A => Boolean) = {
       m match {
-        case DLeaf(a) => Set(Some(a))
-        case DSuccess => Set(None)
-        case DFail => Set()
-        case DOr(l,r) => toSet(l) | toSet(r)
-        case DAssert(l,r) => if (eval(l)) toSet(r) else Set()
-        case DAssertNot(l,r) => if (!eval(l)) toSet(r) else Set()
-        case DLft(_) => throw new Exception("toSet operetor is undefined for DLft.")
+        case DLeaf(a) => v(a)
+        case DSuccess() => true
+        case DFail() => false
+        case DOr(l,r) => eval(l)(v) || eval(r)(v)
+        case DAssert(l,r) => eval(l)(v) && eval(r)(v)
+        case DAssertNot(l,r) => !eval(l)(v) && eval(r)(v)
+        case DLft(_) => throw new Exception("eval operetor is undefined for DLft.")
       }
     }
 
-    def eval(m: DTree[Nothing,Nothing]) = {
+    def evalr[A](m: DTree[Nothing,A])(v: A => Boolean) = {
       m match {
-        case DLeaf(_) => throw new Exception("eval operetor is undefined for DLeaf.")
-        case DSuccess => true
-        case DFail => false
-        case DOr(l,r) => eval(l) || eval(r)
-        case DAssert(l,r) => eval(l) && eval(r)
-        case DAssertNot(l,r) => !eval(l) && eval(r)
+        case DLeaf(a) => v(a)
+        case DSuccess() => true
+        case DFail() => false
+        case DOr(l,r) => evalr(l)(v) || evalr(r)(v)
+        case DAssert(l,r) => eval(l)(_ => true) && evalr(r)(v)
+        case DAssertNot(l,r) => !eval(l)(_ => true) && evalr(r)(v)
         case DLft(_) => throw new Exception("eval operetor is undefined for DLft.")
+      }
+    }
+
+    def leaves[A](m: DTree[A,A]) = {
+      m match {
+        case DLeaf(a) => Set(a)
+        case DSuccess() | DFail() => Set()
+        case DOr(l,r) => leaves(l) | leaves(r)
+        case DAssert(l,r) => leaves(l) | leaves(r)
+        case DAssertNot(l,r) => leaves(l) | leaves(r)
+        case DLft(_) => throw new Exception("toSet operetor is undefined for DLft.")
       }
     }
   }
